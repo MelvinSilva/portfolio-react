c54220e4581bb583ab4e8db51f7a3f679a341900
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-03-27 21:51:13 +0200

    card

--- src/components/MyProjects.js
+++ src/components/MyProjects.js
@@ -3,19 +3,21 @@ import React from 'react';
 const MyProjects = () => {
     return (
         <div className='my-projects' id="mes-projets">
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            <h1>Mes projets</h1>
-            
-            
+            <div className='card'>
+                <img src="/moodmood.png" alt="" />
+                <h2>Mood Moove</h2>
+                <p>Description du projet</p>
+            </div>
+            <div className='card'>
+                <img src="/wikiplayer.png" alt="" />
+                <h2>Wiki-Player</h2>
+                <p>Description du projet</p>
+            </div>
+            <div className='card'>
+                <img src="/blogcrypto.png" alt="" />
+                <h2>Blog Crypto</h2>
+                <p>Description du projet</p>
+            </div>
 
         </div>
     );

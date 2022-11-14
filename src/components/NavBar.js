02dfa56c4c545b829ddaf9ff280a68f591113991
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-11-14 15:28:55 +0100

    update presentation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,21 +6,23 @@ import { Spin as Hamburger } from 'hamburger-react'
 const Navbar = () => {
     const [burger, setBurger] = useState(false)
 
-    // const handleClick = () => {
-    // setBurger(!burger)
-    // }
+
+    const handleClick = () => {
+    setBurger(!burger)
+    }
+
     return (
 
         <div className='navbar'>
 
             <ul className="navbar-normal">
-                <a href="#home"><li>HOME</li></a>
+                <a  href="#home"><li>HOME</li></a>
                 <a href="#stack"><li>STACK</li></a>
                 <a href="#mes-projets"><li>PROJETS</li></a>
                 <a href="#contact"><li>CONTACT</li></a>
             </ul>
 
-            {burger && <ul className="navbar-burger">
+            {burger && <ul className="navbar-burger" onClick={handleClick}>
                 <a href="#home"><li>HOME</li></a>
                 <a href="#stack"><li>STACK</li></a>
                 <a href="#mes-projets"><li>PROJETS</li></a>
@@ -28,8 +30,6 @@ const Navbar = () => {
             </ul>}
             <div className="navbar-burger"><Hamburger toggled={burger} toggle={setBurger} />
             </div>
-
-
         </div>
     );
 };
--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React from 'react';
 import TypeAnimation from 'react-type-animation';
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from 'react-animation-on-scroll';
 
 
 const Presentation = () => {
@@ -17,7 +19,20 @@ const Presentation = () => {
                     className="bounce-in-top"
                     repeat={Infinity}
                 />
-                <p>Je suis<br /> <span>développeur</span> <span>front-end</span><br />  <i><strike>mais aussi</strike></i> <br /> <span>designer</span> à Paris.</p>
+
+                <AnimationOnScroll animateIn="animate__tada">
+                <p>Je suis</p>
+                </AnimationOnScroll>
+                <AnimationOnScroll delay={500} animateIn="animate__fadeInLeftBig">
+                <p><span>développeur</span> <span>front-end</span></p>
+                </AnimationOnScroll>
+                <AnimationOnScroll delay={2000} animateIn="animate__wobble">
+                <p><i><strike>mais aussi</strike></i></p>
+                </AnimationOnScroll>
+                <AnimationOnScroll delay={3000} animateIn="animate__fadeInLeftBig">
+                <p><span>designer</span> à Paris.</p>
+                </AnimationOnScroll>
+                {/* <p>Je suis<br /> <span>développeur</span> <span>front-end</span><br />  <i><strike>mais aussi</strike></i> <br /> <span>designer</span> à Paris.</p> */}
                 {/* <br/>
                 <p className='description'>Issue d'une formation <span>diplômante</span>, j'ai pu apprendre des <span>langages de programmation</span> pour le développement web &#40;voir ici&#41; et construire des <span>API</span> afin de manipuler des données.
                 Depuis quelques mois, je suis <span>développeur Full stack</span> chez SNCF. Je monte en <span>compétence</span> et en <span>experience</span> sur <span>Angular</span>, <span>Java</span> et son framework <span>Spring Boot</span>. J'ai tout de même une affection plus particulière pour le dev <span>font-end</span> et notamment la lib <span>React</span> sur laquelle je me perfectionne chaque jour en réalisant des projets perso. Mes connaissances transverses dans le <span>graphisme</span> me donne une approche réaliste que peut avoir un client afin <span>d'optimiser l'UI et l'UX</span> d'une application web. 

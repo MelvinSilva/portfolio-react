fab8726ed36026d526633004a8b8ac71d5eb1e50
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-11-27 20:03:25 +0100

    stroke

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -25,7 +25,11 @@ const Presentation = () => {
                     <IoDesktopOutline /> <IoColorPaletteOutline />
                 </AnimationOnScroll>
                 <p className='description'>I like to build amazing UI/UX experiences for humans 🚀 <br />Currently I am working on a mission for a large railway company in Paris on Angular technology with TypeScript 💻</p>
-                <a href="#stack" class="arrow down">Scroll</a>
+                <svg class="arrows">
+                    <path class="a1" d="M0 0 L30 32 L60 0"></path>
+                    <path class="a2" d="M0 20 L30 52 L60 20"></path>
+                    <path class="a3" d="M0 40 L30 72 L60 40"></path>
+                </svg>
             </div>
         </div>
 

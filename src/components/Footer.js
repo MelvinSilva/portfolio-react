9b99190e8a9c290a71e29f0ffcacd1de0d867598
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-12-04 20:42:28 +0100

    copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
     return (
         <div className='footer'>
-            <p>Made with ❤️ by Melvin Silva - 2022</p>
+            <p>Made with 💜 by Melvin Silva - © 2022 All rights reserved</p>
 
         </div>
     );

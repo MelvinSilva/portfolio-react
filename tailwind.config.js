ee48297fd0ec8059b1fad1a5143700e263ef5265
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-12-29 17:31:57 +0100

    add info card project

--- tailwind.config.js
+++ tailwind.config.js
@@ -78,12 +78,12 @@ module.exports = {
         },
       },
       animation: {
-        'gradient-background-1': 'gradient-background-1 5s infinite',
-        'gradient-foreground-1': 'gradient-foreground-1 5s infinite',
-        'gradient-background-2': 'gradient-background-2 5s infinite',
-        'gradient-foreground-2': 'gradient-foreground-2 5s infinite',
-        'gradient-background-3': 'gradient-background-3 5s infinite',
-        'gradient-foreground-3': 'gradient-foreground-3 5s infinite',
+        'gradient-background-1': 'gradient-background-1 4s infinite',
+        'gradient-foreground-1': 'gradient-foreground-1 4s infinite',
+        'gradient-background-2': 'gradient-background-2 4s infinite',
+        'gradient-foreground-2': 'gradient-foreground-2 4s infinite',
+        'gradient-background-3': 'gradient-background-3 4s infinite',
+        'gradient-foreground-3': 'gradient-foreground-3 4s infinite',
       }
     },
   },

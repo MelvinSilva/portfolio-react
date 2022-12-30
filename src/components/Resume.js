580943013d3fd802b528c1371320c3d8864e5ddd
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2022-12-30 03:28:35 +0100

    responsive desing 1400px

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -14,14 +14,14 @@ const Resume = () => {
 
     return (
         <div className='resume'>
-            <input type="button" value={t('resume-text')} onClick={togglePopup} className="resume-cv" />
+            {/* <input type="button" value={t('resume-text')} onClick={togglePopup} className="resume-cv" />
             {isOpen &&
                 <Popup
                     content={<>
                         <img src="cv-melvin.png" alt="cv-melvin" />
                     </>}
                     handleClose={togglePopup}
-                />}
+                />} */}
             <div className="resume-cv2"><a href="https://drive.google.com/file/d/1Ar679TjwQAVSJs7_nHEN2LHaWUE75_2F/view?usp=share_link" donwload target="_blank" rel="noopener noreferrer">{t('resume-download')}</a>
             </div>
         </div>

00193fd754694a0cbb3f5bd1e36eb0edc1fb527c
MelvinSilva <93677666+MelvinSilva@users.noreply.github.com>
2023-02-02 14:19:09 +0100

    test title

--- src/components/Presentation.js
+++ src/components/Presentation.js
@@ -2,7 +2,6 @@
 /* eslint-disable jsx-a11y/iframe-has-title */
 import React, { useContext } from 'react';
 import "animate.css/animate.min.css";
-import { AnimationOnScroll } from 'react-animation-on-scroll';
 import { useTranslation } from "react-i18next";
 import { ThemeContext } from '../contexts/ThemeContext';
 import Resume from './Resume';
@@ -17,6 +16,18 @@ const Presentation = () => {
         <div>
             <div className={theme ? 'presentation' : 'presentation light'}>
                 <p className='name'>{t('presentation.title')}</p>
+                <h1 class="py-2 text-[8vw] text-center sm:text-10xl leading-none select-none tracking-tightest font-extrabold">
+                    <span data-content="Melvin Silva" class="font_text relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-0 before:bottom-0 before:left-0 before:text-center before:text-gray-300 before:animate-gradient-background-1">
+                        <span class="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-1-start to-gradient-1-end animate-gradient-foreground-1">Melvin Silva</span>
+                    </span>
+                    <span data-content={t('presentation.description')} class="font_text relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-0 before:bottom-0 before:left-0 before:text-center before:text-gray-300 before:animate-gradient-background-2">
+                        <span class="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-2-start to-gradient-2-end animate-gradient-foreground-2">{t('presentation.description')}</span>
+                    </span>
+                   
+                    <span data-content={t('presentation.description2')} class="font_text relative block before:content-[attr(data-content)] before:w-full before:z-0 before:block before:absolute before:top-0 before:px-0 before:bottom-0 before:left-0 before:text-center before:text-gray-300 before:animate-gradient-background-3">
+                        <span class="px-2 text-transparent bg-clip-text bg-gradient-to-r from-gradient-3-start to-gradient-3-end animate-gradient-foreground-3">{t('presentation.description2')}</span>
+                    </span>
+                </h1>
                 {/* <AnimationOnScroll animateIn="animate__tada">
                     <p className='title-presentation'><span>Melvin Silva</span></p>
                 </AnimationOnScroll>

90bd11f4fe69c3f52f7e0134c41a5ecc43a9ec6b
Melvin SILVA <melvin.silva@sncf.fr>
2023-04-24 20:45:46 +0200

    fix button whatsapp responsive

--- src/components/WhatsappButton.js
+++ src/components/WhatsappButton.js
@@ -1,7 +1,10 @@
 import React from "react";
+import { useSpring, animated } from "react-spring";
 import { ImWhatsapp } from "react-icons/im";
+import { useState, useEffect } from "react";
 
 const WhatsappButton = () => {
+  const [bounce, setBounce] = useState(false);
   const phoneNumber = "33770012972"; // Remplacez ce numéro par votre numéro de téléphone
   const whatsappLink = `https://wa.me/${phoneNumber}`;
 
@@ -9,13 +12,27 @@ const WhatsappButton = () => {
     window.open(whatsappLink, "popup", "width=600,height=600");
   };
 
+  const props = useSpring({
+    transform: bounce ? "scale(1.2)" : "scale(1)",
+    config: { mass: 5, tension: 200, friction: 4 },
+  });
+
+  useEffect(() => {
+    setBounce(true);
+  }, []);
+
   return (
-    <a href={whatsappLink} className="whatsapp-button" onClick={handleClick}>
+    <animated.a
+      href={whatsappLink}
+      className="whatsapp-button"
+      onClick={handleClick}
+      style={props}
+    >
       <div className="whatsapp-icon">
         <ImWhatsapp />
       </div>
-      <span className="whatsapp-text">Contact via WhatsApp</span>
-    </a>
+      <span className="whatsapp-text">Contactez-moi</span>
+    </animated.a>
   );
 };
 
